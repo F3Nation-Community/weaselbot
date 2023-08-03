8668675f96e982a1e8ae8a7070b32cebc0534915
Ken Sipe <kensipe@gmail.com>
2023-08-03 12:03:55 -0500

    Adding setup.py file
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    with open(filename) as f:
+        lines = (line.strip() for line in f)
+        return [line for line in lines if line and not line.startswith("#")]
+
+setup(
+    name="Weasel Bot",
+    version="0.1.0",
+    author="Evan Petzoldt",
+    description="WeaselBot is a Slack bot designed for F3 to get the PAX in your region more engaged",
+    url="https://github.com/evanpetzoldt/weaselbot",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=parse_requirements("requirements.txt"),
+)
