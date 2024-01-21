c1fd93d260ba133894c2a89bbd93b3dfd25f3106
James Sheldon <jimsheldon@icloud.com>
2024-01-21 06:32:23 -0600

    modularized, optimized, tested

--- kotter_report.py
+++ kotter_report.py
@@ -9,9 +9,8 @@ from slack_sdk import WebClient
 from sqlalchemy import MetaData, Table
 from sqlalchemy.sql import func, select, case, or_, and_
 
-from f3_data_builder import mysql_connection
+from weaselbot.f3_data_builder import mysql_connection
 
-engine = mysql_connection()
 
 NO_POST_THRESHOLD = 2
 REMINDER_WEEKS = 2
@@ -19,100 +18,123 @@ HOME_AO_CAPTURE = datetime.combine(date.today() + timedelta(weeks=-8), datetime.
 NO_Q_THRESHOLD_WEEKS = 4
 NO_Q_THRESHOLD_POSTS = 4
 
-engine = mysql_connection()
-metadata = MetaData()
-metadata.reflect(engine, schema="weaselbot")
+
+def slack_client(token: str) -> WebClient:
+    """Instantiate Slack Web client"""
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    return WebClient(token=token, ssl=ssl_context)
+
+
+def col_cleaner(s: pd.Series) -> pd.Series:
+    try:
+        return s.str.strip()
+    except AttributeError:
+        return s
 
 
 def build_kotter_report(df_posts: pd.DataFrame, df_qs: pd.DataFrame, siteq: str) -> str:
-    # Build Slack message
-    sMessage = f"Howdy, <@{siteq}>! This is your weekly WeaselBot Site Q report. According to my records..."
+    """Build Slack SiteQ message"""
+    sMessage = [f"Howdy, <@{siteq}>! This is your weekly WeaselBot Site Q report. According to my records...",]
 
     if len(df_posts) > 0:
-        sMessage += "\n\nThe following PAX haven't posted in a bit. \
-Now may be a good time to reach out to them when you get a minute. No OYO! :muscle:"
+        sMessage.append("\n\nThe following PAX haven't posted in a bit.")
+        sMessage.append("Now may be a good time to reach out to them when you get a minute. No OYO! :muscle:")
 
-        for _, row in df_posts.iterrows():
-            sMessage += "\n- <@" + row["pax_id"] + ">"
+        for row in df_posts.itertuples(index=False):
+            sMessage.append(f"\n- <@{row.pax_id}>")
 
     if len(df_qs) > 0:
-        sMessage += "\n\nThese guys haven't Q'd anywhere in a while (or at all!):"
+        sMessage.append("\n\nThese guys haven't Q'd anywhere in a while (or at all!):")
 
-        for _, row in df_qs.iterrows():
-            sMessage += "\n- <@" + row["pax_id"] + ">"
-            if np.isnan(row["days_since_last_q"]):
-                sMessage += " (no Q yet!)"
+        for row in df_qs.itertuples(index=False):
+            sMessage.append(f"\n- <@{row.pax_id}>")
+            if np.isnan(row.days_since_last_q):
+                sMessage.append(" (no Q yet!)")
             else:
-                sMessage += " (" + str(int(row["days_since_last_q"])) + " days since last Q)"
-    return sMessage
+                sMessage.append(f" ({str(int(row.days_since_last_q))} days since last Q)")
+    return "".join(sMessage)
 
 
-bd = metadata.tables["weaselbot.combined_attendance"]
-u = metadata.tables["weaselbot.combined_users"]
-b = metadata.tables["weaselbot.combined_beatdowns"]
-a = metadata.tables["weaselbot.combined_aos"]
-r = metadata.tables["weaselbot.regions"]
+def nation_select(metadata):
+    bd = metadata.tables["weaselbot.combined_attendance"].alias("bd")
+    u = metadata.tables["weaselbot.combined_users"].alias("u")
+    b = metadata.tables["weaselbot.combined_beatdowns"].alias("b")
+    a = metadata.tables["weaselbot.combined_aos"].alias("a")
 
-q_flag = case((or_(bd.c.user_id == b.c.q_user_id, bd.c.user_id == b.c.coq_user_id), 1), else_=0).label("q_flag")
+    q_flag = case((or_(bd.c.user_id == b.c.q_user_id, bd.c.user_id == b.c.coq_user_id), 1), else_=0).label("q_flag")
 
-nation_select = select(
-    u.c.email,
-    a.c.ao_id,
-    a.c.ao_name.label("ao"),
-    b.c.bd_date.label("date"),
-    func.year(b.c.bd_date).label("year_num"),
-    func.month(b.c.bd_date).label("month_num"),
-    func.week(b.c.bd_date).label("week_num"),
-    func.day(b.c.bd_date).label("day_num"),
-    q_flag,
-)
-nation_select = nation_select.select_from(
-    bd.join(u, u.c.user_id == bd.c.user_id).join(b, bd.c.beatdown_id == b.c.beatdown_id).join(a, b.c.ao_id == a.c.ao_id)
-)
-nation_select = nation_select.where(and_(b.c.bd_date > 0, b.c.bd_date <= func.curdate()))
+    sql = select(
+        u.c.email,
+        a.c.ao_id,
+        a.c.ao_name.label("ao"),
+        b.c.bd_date.label("date"),
+        func.year(b.c.bd_date).label("year_num"),
+        func.month(b.c.bd_date).label("month_num"),
+        func.week(b.c.bd_date).label("week_num"),
+        func.day(b.c.bd_date).label("day_num"),
+        q_flag,
+    )
+    sql = sql.select_from(
+        bd.join(u, u.c.user_id == bd.c.user_id)
+        .join(b, bd.c.beatdown_id == b.c.beatdown_id)
+        .join(a, b.c.ao_id == a.c.ao_id)
+    )
+    return sql.where(and_(b.c.bd_date > 0, b.c.bd_date <= func.curdate()))
 
 
-dtypes = dict(
-    id=pd.Int16Dtype(),
-    paxminer_schema=pd.StringDtype(),
-    slack_token=pd.StringDtype(),
-    send_achievements=pd.Int16Dtype(),
-    send_aoq_reports=pd.Int16Dtype(),
-    achievement_channel=pd.StringDtype(),
-    default_siteq=pd.StringDtype(),
-)
-df_regions = pd.read_sql(select(r).where(r.c.send_aoq_reports == 1), engine, dtype=dtypes)
+def region_select(metadata):
+    r = metadata.tables["weaselbot.regions"]
+    return select(r).where(r.c.send_aoq_reports == 1)
 
-dtypes = dict(
-    email=pd.StringDtype(),
-    ao_id=pd.StringDtype(),
-    ao=pd.StringDtype(),
-    year_num=pd.Int16Dtype(),
-    month_num=pd.Int16Dtype(),
-    week_num=pd.Int16Dtype(),
-    day_num=pd.Int16Dtype(),
-    q_flag=pd.Int16Dtype(),
-)
-nation_df = pd.read_sql(nation_select, engine, parse_dates="date", dtype=dtypes)
 
-for region_row in df_regions.itertuples(index=False):
-    slack_secret = region_row.slack_token
+def region_df(sql, engine):
+    dtypes = dict(
+        id=pd.Int16Dtype(),
+        paxminer_schema=pd.StringDtype(),
+        slack_token=pd.StringDtype(),
+        send_achievements=pd.Int16Dtype(),
+        send_aoq_reports=pd.Int16Dtype(),
+        achievement_channel=pd.StringDtype(),
+        default_siteq=pd.StringDtype(),
+    )
+    df = pd.read_sql(sql, engine, dtype=dtypes)
+    df = df.apply(col_cleaner)
 
-    print(f"running {region_row.paxminer_schema}...")
+    return df
 
-    t = Table("users", metadata, autoload_with=engine, schema=region_row.paxminer_schema)
-    ao = Table("aos", metadata, autoload_with=engine, schema=region_row.paxminer_schema)
 
-    user_df = pd.read_sql(select(t), engine, dtype=pd.StringDtype(), parse_dates="start_date")
-    user_df.app = user_df.app.astype(pd.Int64Dtype())
-    df_siteq = pd.read_sql(select(ao.c.ao, ao.c.site_q_user_id), engine, dtype=pd.StringDtype())
+def nation_df(sql, engine):
+    dtypes = dict(
+        email=pd.StringDtype(),
+        ao_id=pd.StringDtype(),
+        ao=pd.StringDtype(),
+        year_num=pd.Int16Dtype(),
+        month_num=pd.Int16Dtype(),
+        week_num=pd.Int16Dtype(),
+        day_num=pd.Int16Dtype(),
+        q_flag=pd.Int16Dtype(),
+    )
+    df = pd.read_sql(sql, engine, parse_dates="date", dtype=dtypes)
+    df = df.apply(col_cleaner)
+    df.email = df.email.str.lower().replace("none", pd.NA)
 
-    with engine.begin() as cnxn:
-        paxminer_log_channel = cnxn.execute(select(ao.c.channel_id).where(ao.c.ao == 'paxminer_logs')).scalar()
+    return df
 
-    df = pd.merge(nation_df, user_df, how="inner", on="email")
-    df.rename({"user_id": "pax_id", "user_name": "pax_name"}, axis=1, inplace=True)
 
+def pull_region_users(row, engine, metadata):
+    users = Table("users", metadata, autoload_with=engine, schema=row.paxminer_schema)
+    df = pd.read_sql(select(users), engine, dtype=pd.StringDtype(), parse_dates="start_date")
+    df.app = df.app.astype(pd.Int64Dtype())
+    df = df.apply(col_cleaner)
+    df.email = df.email.str.lower().replace("none", pd.NA)
+    df.phone = df.phone.replace("", pd.NA)
+
+    return df
+
+
+def add_home_ao(df):
     home_ao_df = (
         df.loc[df.date > HOME_AO_CAPTURE]
         .groupby(["pax_id"])["ao"]
@@ -124,24 +146,34 @@ for region_row in df_regions.itertuples(index=False):
         .rename({"ao": "home_ao"}, axis=1)
     )
 
-    df = pd.merge(df, home_ao_df, how="left")
-    df.home_ao.fillna("unknown", inplace=True)
-    df2 = (
+    return pd.merge(df, home_ao_df, how="left").dropna(subset="home_ao")
+
+
+def pax_appearances(df):
+    df1 = df.groupby(["year_num", "week_num"], as_index=False)["date"].min()
+    df2 = df.groupby(["pax_id", "home_ao"], as_index=False)["ao"].count()
+    return pd.merge(df2, df1, how="cross").drop("ao", axis=1)
+
+
+def clean_data(df, row, engine, metadata):
+    df.rename({"user_id": "pax_id", "user_name": "pax_name"}, axis=1, inplace=True)
+
+    df = add_home_ao(df)
+
+    df_non_zero = (
         df.groupby(["year_num", "week_num", "pax_id", "home_ao"], as_index=False)
         .agg({"day_num": np.count_nonzero})
         .rename(columns={"day_num": "post_count"})
     )
-    df3 = df.groupby(["year_num", "week_num"], as_index=False)["date"].min()
-    df4 = df.groupby(["pax_id", "home_ao"], as_index=False)["ao"].count()
-    df5 = pd.merge(df4, df3, how="cross").drop("ao", axis=1)
+    df_appearances = pax_appearances(df)
     df6 = (
-        pd.merge(df5, df2, how="left")
+        pd.merge(df_appearances, df_non_zero, how="left")
         .dropna(
             subset=[
                 "date",
             ]
         )
-        .fillna(0)
+        .fillna(pd.NA)
         .sort_values(["pax_id", "date"])
         .assign(
             post_count_rolling=lambda x: x.post_count.rolling(NO_POST_THRESHOLD, min_periods=1).sum(),
@@ -150,12 +182,14 @@ for region_row in df_regions.itertuples(index=False):
             ).sum(),
         )
     )
+    df6.post_count_rolling = df6.post_count_rolling.astype(pd.Float64Dtype())
+    df6.post_count_rolling_stop = df6.post_count_rolling_stop.astype(pd.Float64Dtype())
 
     # Pull list of guys not posting
     pull_week = df6.loc[df6.date < datetime.today()][
         "date"
     ].max()  # this will only work as expected if you run on Sunday because of the rolling count
-    df7 = df6[(df6["post_count_rolling"] == 0) & (df6["date"] == pull_week) & (df6["post_count_rolling_stop"] > 0)]
+    df7 = df6[(df6["post_count_rolling"].isna()) & (df6["date"] == pull_week) & (df6["post_count_rolling_stop"] > 0)]
 
     # Pull pull list of guys not Q-ing
     df8 = (
@@ -165,6 +199,7 @@ for region_row in df_regions.itertuples(index=False):
         .rename(columns={"date": "last_q_date"})
         .assign(days_since_last_q=lambda x: (datetime.today() - x.last_q_date).dt.days)
     )
+    df8.days_since_last_q = df8.days_since_last_q.astype(pd.Int64Dtype())
     df9 = pd.merge(df6, df8, how="left")
     df10 = df9[
         (df9["post_count_rolling"] > 0)
@@ -176,25 +211,26 @@ for region_row in df_regions.itertuples(index=False):
     ]
 
     # Merge siteq list
-    df_posts = pd.merge(df7, df_siteq, how="left", left_on="home_ao", right_on="ao").loc[
-        lambda x: x.home_ao != "unknown"
-    ]
+    ao = Table("aos", metadata, autoload_with=engine, schema=row.paxminer_schema)
+
+    df_siteq = pd.read_sql(select(ao.c.ao, ao.c.site_q_user_id), engine, dtype=pd.StringDtype()).dropna()
+    df_posts = pd.merge(df7, df_siteq, how="left", left_on="home_ao", right_on="ao")
+    df_posts = df_posts.loc[df_posts.home_ao.notna()]
+
     df_qs = pd.merge(
         df10, df_siteq, how="left", left_on="home_ao", right_on="ao"
     )  # remove NAs... these are guys who haven't posted to a regular AO in the home_ao period
 
     for df_ in [df_posts, df_qs]:
         mask = df_["site_q_user_id"].isna()
-        df_.loc[mask, "site_q_user_id"] = region_row.default_siteq
+        df_.loc[mask, "site_q_user_id"] = row.default_siteq
 
-    # instantiate Slack client
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-    slack_client = WebClient(slack_secret, ssl=ssl_context)
+    return df_siteq, df_posts, df_qs
 
+
+def send_weaselbot_report(row, client, df_siteq, df_posts, df_qs):
     # Loop through site-qs that have PAX on the list and send the weaselbot report
-    for siteq in df_siteq["site_q_user_id"].unique():  # df8['site_q'].unique():
+    for siteq in df_siteq["site_q_user_id"].unique():
         dftemp_posts = df_posts[df_posts["site_q_user_id"] == siteq]
         dftemp_qs = df_qs[df_qs["site_q_user_id"] == siteq]
 
@@ -204,37 +240,66 @@ for region_row in df_regions.itertuples(index=False):
         # Send message
         if (len(dftemp_posts) + len(dftemp_qs)) > 0:
             try:
-                response = slack_client.chat_postMessage(channel=siteq, text=sMessage, link_names=True)
+                client.chat_postMessage(channel=siteq, text=sMessage, link_names=True)
                 print(f"Sent {siteq} this message:\n\n{sMessage}\n\n")
             except Exception as e:
                 print(f"Error sending message to {siteq}: {e}")
 
-    sMessage = build_kotter_report(df_posts, df_qs, region_row.default_siteq)
+    sMessage = build_kotter_report(df_posts, df_qs, row.default_siteq)
     sMessage += "\n\nNote: If you have listed your site Qs on your aos table, this information will have gone out to them as well."
     try:
-        response = slack_client.chat_postMessage(channel=region_row.default_siteq, text=sMessage, link_names=True)
-        print(f'Sent {region_row.default_siteq} this message:\n\n{sMessage}\n\n')
+        if row.default_siteq not in df_siteq["site_q_user_id"].unique().tolist():
+            client.chat_postMessage(channel=row.default_siteq, text=sMessage, link_names=True)
+            print(f'Sent {row.default_siteq} this message:\n\n{sMessage}\n\n')
     except Exception as e:
         print(f"hit exception {e}")
         print(e.response)
         if e.response.get("error") == "not_in_channel":
             try:
                 print("trying to join channel")
-                slack_client.conversations_join(channel=region_row.default_siteq)
-                response = slack_client.chat_postMessage(
-                    channel=region_row.default_siteq, text=sMessage, link_names=True
-                )
+                client.conversations_join(channel=row.default_siteq)
+                client.chat_postMessage(channel=row.default_siteq, text=sMessage, link_names=True)
                 print("sent this message:\n\n{sMessage}\n\n")
             except Exception as e:
                 print("hit exception joining channel")
 
-    # Send myself a message
-    separator = ", "
+
+def notify_yhc(row, engine, metadata, client):
+    ao = metadata.tables[f"{row.paxminer_schema}.aos"]
+    with engine.begin() as cnxn:
+        paxminer_log_channel = cnxn.execute(select(ao.c.channel_id).where(ao.c.ao == 'paxminer_logs')).scalar()
     try:
-        response2 = slack_client.chat_postMessage(channel=paxminer_log_channel, text="Successfully sent kotter reports")
+        client.chat_postMessage(channel=paxminer_log_channel, text="Successfully sent kotter reports")
         print(f"Sent {paxminer_log_channel} this message:\n\nSuccessfully sent kotter reports\n\n")
     except Exception as e:
         print(f"Error sending message to {paxminer_log_channel}: {e}")  # TODO: add self to channel
     print("All done!")
 
-engine.dispose()
+
+def main():
+    engine = mysql_connection()
+    metadata = MetaData()
+    metadata.reflect(engine, schema="weaselbot")
+
+    nation_sql, region_sql = nation_select(metadata), region_select(metadata)
+
+    df_nation, df_regions = nation_df(nation_sql, engine), region_df(region_sql, engine)
+
+    for row in df_regions.itertuples(index=False):
+        print(f"running {row.paxminer_schema}...")
+        user_df = pull_region_users(row, engine, metadata)
+        df = pd.merge(df_nation, user_df, how="inner", on="email").dropna(subset='email')
+
+        df_siteq, df_posts, df_qs = clean_data(df, row, engine, metadata)
+
+        token = row.slack_token
+        client = slack_client(token)
+        send_weaselbot_report(row, client, df_siteq, df_posts, df_qs)
+
+        notify_yhc(row, engine, metadata, client)
+
+    engine.dispose()
+
+
+if __name__ == "__main__":
+    main()
