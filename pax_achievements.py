62c8d8a51daea3df3249b74d7b227de7d96b774e
James Sheldon <jimsheldon@icloud.com>
2024-05-13 18:06:44 -0500

    pax_achievements_v3

--- pax_achievements.py
+++ pax_achievements.py
@@ -14,28 +14,28 @@ from utils import mysql_connection, send_to_slack
 
 def home_region_sub_query(u, a, b, ao, date_range):
     """
-    Abstract the subquery needed for length of time to 
+    Abstract the subquery needed for length of time to
     look back for considering the home region. This is
     needed because there are many scenarios where a man
-    could lapse in attending F3. Many different checks 
+    could lapse in attending F3. Many different checks
     should be considered before defaulting to the maximium
     date range
     """
     subquery = select(u.c.email, func.count(a.c.user_id).label("attendance"))
-    subquery = subquery.select_from(u.join(a, a.c.user_id == u.c.user_id)
-                          .join(b, and_(a.c.q_user_id == b.c.q_user_id,
-                                        a.c.ao_id == b.c.ao_id,
-                                        a.c.date == b.c.bd_date))
-                          .join(ao, b.c.ao_id == ao.c.channel_id)
-                         )
+    subquery = subquery.select_from(
+        u.join(a, a.c.user_id == u.c.user_id)
+        .join(b, and_(a.c.q_user_id == b.c.q_user_id, a.c.ao_id == b.c.ao_id, a.c.date == b.c.bd_date))
+        .join(ao, b.c.ao_id == ao.c.channel_id)
+    )
     subquery = subquery.where(func.datediff(func.curdate(), b.c.bd_date) < date_range)
     subquery = subquery.group_by(u.c.email).subquery()
     return subquery
 
+
 def build_home_regions(schemas, metadata, engine):
     queries = []
     for row in schemas:
-        # some men don't post in the immediate prior 30 days. To account for this, 
+        # some men don't post in the immediate prior 30 days. To account for this,
         # a full year look back is performed. If there are 0 posts in the immediately prior
         # 30 days, the lookback is used. This is a nieve approach and there are
         # edge cases that need to be accounted for.
@@ -51,22 +51,26 @@ def build_home_regions(schemas, metadata, engine):
 
             s1, s2, s3, s4 = (home_region_sub_query(u, a, b, ao, date_range) for date_range in (30, 60, 90, 120))
 
-            sql = select(literal_column(f"'{schema}'").label("region"), 
-                        u.c.email,
-                        case((s1.c.attendance != None, s1.c.attendance),
-                            (s2.c.attendance != None, s2.c.attendance),
-                            (s3.c.attendance != None, s3.c.attendance),
-                            (s4.c.attendance != None, s4.c.attendance),
-                            else_=func.count(a.c.user_id)).label("attendance"))
-            sql = sql.select_from(u.join(a, a.c.user_id == u.c.user_id)
-                                .join(b, and_(a.c.q_user_id == b.c.q_user_id,
-                                                a.c.ao_id == b.c.ao_id,
-                                                a.c.date == b.c.bd_date))
-                                .join(ao, b.c.ao_id == ao.c.channel_id)
-                                .outerjoin(s1, u.c.email == s1.c.email)
-                                .outerjoin(s2, u.c.email == s2.c.email)
-                                .outerjoin(s3, u.c.email == s3.c.email)
-                                .outerjoin(s4, u.c.email == s4.c.email))
+            sql = select(
+                literal_column(f"'{schema}'").label("region"),
+                u.c.email,
+                case(
+                    (s1.c.attendance != None, s1.c.attendance),
+                    (s2.c.attendance != None, s2.c.attendance),
+                    (s3.c.attendance != None, s3.c.attendance),
+                    (s4.c.attendance != None, s4.c.attendance),
+                    else_=func.count(a.c.user_id),
+                ).label("attendance"),
+            )
+            sql = sql.select_from(
+                u.join(a, a.c.user_id == u.c.user_id)
+                .join(b, and_(a.c.q_user_id == b.c.q_user_id, a.c.ao_id == b.c.ao_id, a.c.date == b.c.bd_date))
+                .join(ao, b.c.ao_id == ao.c.channel_id)
+                .outerjoin(s1, u.c.email == s1.c.email)
+                .outerjoin(s2, u.c.email == s2.c.email)
+                .outerjoin(s3, u.c.email == s3.c.email)
+                .outerjoin(s4, u.c.email == s4.c.email)
+            )
             sql = sql.where(func.year(b.c.bd_date) == func.year(func.curdate()))
             sql = sql.group_by(literal_column(f"'{schema}'").label("region"), u.c.email)
             queries.append(sql)
@@ -74,6 +78,7 @@ def build_home_regions(schemas, metadata, engine):
             print(f"Schema {schema} error.")
 
     return union_all(*queries)
+
 
 def nation_sql(schemas, engine, metadata: MetaData) -> Selectable:
     queries = []
@@ -93,21 +98,27 @@ def nation_sql(schemas, engine, metadata: MetaData) -> Selectable:
                 ao.c.ao.label("ao"),
                 b.c.bd_date.label("date"),
                 case((or_(a.c.user_id == b.c.q_user_id, a.c.user_id == b.c.coq_user_id), 1), else_=0).label("q_flag"),
-                b.c.backblast
+                b.c.backblast,
             )
-            sql = sql.select_from(u.join(a, a.c.user_id == u.c.user_id)
-                                    .join(b, and_(or_(a.c.q_user_id == b.c.q_user_id, a.c.q_user_id == b.c.coq_user_id),
-                                                    a.c.ao_id == b.c.ao_id,
-                                                    a.c.date == b.c.bd_date))
-                                    .join(ao, b.c.ao_id == ao.c.channel_id)
-                                    )
+            sql = sql.select_from(
+                u.join(a, a.c.user_id == u.c.user_id)
+                .join(
+                    b,
+                    and_(
+                        or_(a.c.q_user_id == b.c.q_user_id, a.c.q_user_id == b.c.coq_user_id),
+                        a.c.ao_id == b.c.ao_id,
+                        a.c.date == b.c.bd_date,
+                    ),
+                )
+                .join(ao, b.c.ao_id == ao.c.channel_id)
+            )
 
             sql = sql.where(
-                    func.year(b.c.bd_date) == func.year(func.curdate()),
-                    u.c.email != "none",
-                    u.c.user_name != "PAXminer",
-                    b.c.q_user_id != None,
-                )
+                func.year(b.c.bd_date) == func.year(func.curdate()),
+                u.c.email != "none",
+                u.c.user_name != "PAXminer",
+                b.c.q_user_id != None,
+            )
             queries.append(sql)
         except Exception:
             print(f"Schema {schema} error.")
@@ -115,39 +126,13 @@ def nation_sql(schemas, engine, metadata: MetaData) -> Selectable:
     return union_all(*queries)
 
 
-def region_sql(metadata: MetaData):
-    """Pick out only the regions that want their weaselshaker awards."""
-    t = metadata.tables["weaselbot.regions"]
-    return select(t).where(t.c.send_achievements == 1)
-
-
-def award_view(schema, engine: Engine, metadata: MetaData) -> Selectable:
-    aa = Table("achievements_awarded", metadata, autoload_with=engine, schema=schema)
-    al = Table("achievements_list", metadata, autoload_with=engine, schema=schema)
-
-    sql = (
-        select(aa, al.c.code)
-        .select_from(aa.join(al, aa.c.achievement_id == al.c.id))
-        .where(func.year(aa.c.date_awarded) == func.year(func.curdate()))
-    )
-    return sql
-
-
-def award_list(schema, engine: Engine, metadata: MetaData) -> Selectable:
-    try:
-        al = metadata.tables[f"{schema}.achievements_list"]
-    except KeyError:
-        al = Table("achievements_list", metadata, autoload_with=engine, schema=schema)
-
-    return select(al)
-
-
 def the_priest(df: pl.DataFrame, bb_filter: pl.Expr, ao_filter: pl.Expr) -> pl.DataFrame:
     grouping = ["year", "slack_user_id", "region"]
     x = (
         df.with_columns(pl.col("date").dt.year().alias("year"))
         .filter((bb_filter) | (ao_filter))
-        .group_by(pl.col(grouping)).agg(pl.col("ao_id").count(), pl.col("date").max())
+        .group_by(pl.col(grouping))
+        .agg(pl.col("ao_id").count(), pl.col("date").max())
         .filter(pl.col("ao_id") >= 25)
         .with_columns(pl.col("date").alias("date_awarded"))
         .drop(["ao_id", "date"])
@@ -160,7 +145,8 @@ def the_monk(df: pl.DataFrame, bb_filter: pl.Expr, ao_filter: pl.Expr) -> pl.Dat
     x = (
         df.with_columns(pl.col("date").dt.month().alias("month"))
         .filter((bb_filter) | (ao_filter))
-        .group_by(grouping).agg(pl.col("ao_id").count(), pl.col("date").max())
+        .group_by(grouping)
+        .agg(pl.col("ao_id").count(), pl.col("date").max())
         .filter(pl.col("ao_id") >= 4)
         .with_columns(pl.col("date").alias("date_awarded"))
         .drop(["ao_id", "date"])
@@ -173,7 +159,8 @@ def leader_of_men(df: pl.DataFrame, bb_filter: pl.Expr, ao_filter: pl.Expr) -> p
     x = (
         df.with_columns(pl.col("date").dt.month().alias("month"))
         .filter((pl.col("q_flag") == 1) & (bb_filter) & (ao_filter))
-        .group_by(grouping).agg(pl.col("ao_id").count(), pl.col("date").max())
+        .group_by(grouping)
+        .agg(pl.col("ao_id").count(), pl.col("date").max())
         .filter(pl.col("ao_id") >= 4)
         .with_columns(pl.col("date").alias("date_awarded"))
         .drop(["ao_id", "date"])
@@ -186,7 +173,8 @@ def the_boss(df: pl.DataFrame, bb_filter: pl.Expr, ao_filter: pl.Expr) -> pl.Dat
     x = (
         df.with_columns(pl.col("date").dt.month().alias("month"))
         .filter((pl.col("q_flag") == 1) & (bb_filter) & (ao_filter))
-        .group_by(grouping).agg(pl.col("ao_id").count(), pl.col("date").max())
+        .group_by(grouping)
+        .agg(pl.col("ao_id").count(), pl.col("date").max())
         .filter(pl.col("ao_id") >= 6)
         .with_columns(pl.col("date").alias("date_awarded"))
         .drop(["ao_id", "date"])
@@ -275,13 +263,13 @@ def hdtf(df: pl.DataFrame, bb_filter: pl.Expr, ao_filter: pl.Expr) -> pl.DataFra
     return x
 
 
-def load_to_database(row, engine: Engine, metadata: MetaData, data_to_load: pl.DataFrame) -> None:
+def load_to_database(schema: str, engine: Engine, metadata: MetaData, data_to_load: pl.DataFrame) -> None:
     """after successfully sending Slack notifications, push the data to the `achievements_awarded` table.
     The data frame data_to_load has already been filtered to include only new achievements."""
     try:
-        aa = metadata.tables[f"{row.paxminer_schema}.achievements_awarded"]
+        aa = metadata.tables[f"{schema}.achievements_awarded"]
     except KeyError:
-        aa = Table("achievements_awarded", metadata, autoload_with=engine, schema=row.paxminer_schema)
+        aa = Table("achievements_awarded", metadata, autoload_with=engine, schema=schema)
 
     load_records = data_to_load.to_dicts()
     sql = insert(aa).values(load_records)
@@ -302,15 +290,20 @@ def main():
     nation_query = nation_sql(schemas, engine, metadata)
 
     with engine.begin() as cnxn:
+        logging.info("Building home regions...")
         home_regions = pl.from_pandas(pd.read_sql(home_regions_sql, cnxn))
+        logging.info("Building national beatdown data...")
         nation_df = pl.from_pandas(pd.read_sql(nation_query, cnxn))
 
     nation_df = nation_df.join(home_regions.drop("attendance"), on="email")
     del home_regions
 
     # for QSource, we want to capture only QSource
-    bb_filter = pl.col("backblast").str.slice(0, 100).str.to_lowercase().str.contains(r"q.{0,1}source|q{0,1}[1-9]\.[0-9]\s")
+    bb_filter = (
+        pl.col("backblast").str.slice(0, 100).str.to_lowercase().str.contains(r"q.{0,1}source|q{0,1}[1-9]\.[0-9]\s")
+    )
     ao_filter = pl.col("ao").str.to_lowercase().str.contains(r"q.{0,1}source")
+    logging.info("Building national achievements dataframes...")
 
     dfs = []
     ############# Q Source ##############
@@ -319,7 +312,9 @@ def main():
     ############### END #################
 
     # For beatdowns, we want to exclude QSource and Ruck (blackops too? What is blackops?)
-    bb_filter = ~pl.col("backblast").str.slice(0, 100).str.to_lowercase().str.contains(r"q.{0,1}source|q{0,1}[1-9]\.[0-9]\s")
+    bb_filter = ~pl.col("backblast").str.slice(0, 100).str.to_lowercase().str.contains(
+        r"q.{0,1}source|q{0,1}[1-9]\.[0-9]\s"
+    )
     ao_filter = ~pl.col("ao").str.to_lowercase().str.contains(r"q.{0,1}source|ruck")
 
     ############ ALL ELSE ###############
@@ -331,26 +326,40 @@ def main():
 
     s = posts(nation_df, bb_filter, ao_filter)
     for val in [25, 50, 100, 150, 200]:
-        dfs.append(s.filter(pl.col("ao_id") >= val).with_columns(pl.col("date").alias("date_awarded")).drop(["ao_id", "date"]))
+        dfs.append(
+            s.filter(pl.col("ao_id") >= val).with_columns(pl.col("date").alias("date_awarded")).drop(["ao_id", "date"])
+        )
 
     dfs.append(six_pack(nation_df, bb_filter, ao_filter))
     dfs.append(hdtf(nation_df, bb_filter, ao_filter))
 
+    logging.info("Parsing region info and sending to Slack...")
     for row in schemas:
         schema = row[0]
         ao = Table("aos", metadata, autoload_with=engine, schema=schema)
+
         with engine.begin() as cnxn:
             paxminer_log_channel = cnxn.execute(select(ao.c.channel_id).where(ao.c.ao == "paxminer_logs")).scalar()
-            token = cnxn.execute(select(t.c.slack_token).where(t.c.schema_name == schema)).scalar()
-            channel = cnxn.execute(text(f"SELECT achievement_channel FROM weaselbot.regions WHERE paxminer_schema = '{schema}'")).scalar()
+            token = cnxn.execute(text(f"SELECT slack_token FROM weaselbot.regions WHERE paxminer_schema = '{schema}'")).scalar()
+            channel = cnxn.execute(
+                text(f"SELECT achievement_channel FROM weaselbot.regions WHERE paxminer_schema = '{schema}'")
+            ).scalar()
         if channel is None:
             logging.error(f"{schema} isn't signed up for Weaselbot achievements.")
             continue
         try:
-            awarded = pl.from_pandas(pd.read_sql(
-                award_view(schema, engine, metadata), engine, parse_dates=["date_awarded", "created", "updated"]
-            ))
-            awards = pl.from_pandas(pd.read_sql(award_list(schema, engine, metadata), engine))
+            al = Table("achievements_list", metadata, autoload_with=engine, schema=schema)
+            aa = Table("achievements_awarded", metadata, autoload_with=engine, schema=schema)
+
+            sql = (
+                select(aa, al.c.code)
+                .select_from(aa.join(al, aa.c.achievement_id == al.c.id))
+                .where(func.year(aa.c.date_awarded) == func.year(func.curdate()))
+            )
+
+            with engine.begin() as cnxn:
+                awarded = pl.from_pandas(pd.read_sql(sql, cnxn, parse_dates=["date_awarded", "created", "updated"]))
+                awards = pl.from_pandas(pd.read_sql(select(al), cnxn))
         except NoSuchTableError:
             logging.error(f"{schema} isn't signed up for Weaselbot achievements.")
             continue
@@ -358,7 +367,7 @@ def main():
         if not data_to_load.is_empty():
             load_to_database(schema, engine, metadata, data_to_load)
 
-        logging.info(f"Successfully loaded all records and sent all Slack messages for {row.paxminer_schema}.")
+        logging.info(f"Successfully loaded all records and sent all Slack messages for {schema}.")
 
     engine.dispose()
 
