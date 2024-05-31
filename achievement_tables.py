8e70a0d41483aa31dc0fe4d82ad3160cbb66277f
James Sheldon <jimsheldon@icloud.com>
2024-05-31 04:22:22 -0500

    updated dependencies and added new region

--- achievement_tables.py
+++ achievement_tables.py
@@ -6,7 +6,7 @@ from utils import mysql_connection
 engine = mysql_connection()
 metadata = MetaData()
 
-schema = "f3lehigh_valley"
+schema = "f3neo"
 MYSQL_ENGINE = "InnoDB"
 MYSQL_CHARSET = "utf8mb3"
 MYSQL_COLLATE = "utf8mb3_general_ci"
